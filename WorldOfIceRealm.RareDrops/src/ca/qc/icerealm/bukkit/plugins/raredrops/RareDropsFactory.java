490771a7501a19926f8be48515ade06d4ae88e72
Drask <Drask@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-05 04:40:04 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@47 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

--- WorldOfIceRealm.RareDrops/src/ca/qc/icerealm/bukkit/plugins/raredrops/RareDropsFactory.java
+++ WorldOfIceRealm.RareDrops/src/ca/qc/icerealm/bukkit/plugins/raredrops/RareDropsFactory.java
@@ -4,7 +4,6 @@ import java.util.logging.Logger;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Monster;
-
 import ca.qc.icerealm.bukkit.plugins.common.EntityUtilities;
 
 public class RareDropsFactory 
@@ -64,7 +63,7 @@ public class RareDropsFactory
 			{
 				this.logger.info("Killed Spider");
 				odds.addOddsItem(new RareDropsOddsItem(3.00 * oddsMultiplier, Material.BREAD));
-				odds.addOddsItem(new RareDropsOddsItem(1.00 * oddsMultiplier, Material.COOKIE));
+				odds.addOddsItem(new RareDropsOddsItem(2.00 * oddsMultiplier, Material.COOKIE));
 				odds.addOddsItem(new RareDropsOddsItem(1.00 * oddsMultiplier, Material.POTION));
 				odds.addOddsItem(new RareDropsOddsItem(1.00 * oddsMultiplier, Material.INK_SACK));
 				break;
@@ -100,10 +99,10 @@ public class RareDropsFactory
 				odds.addOddsItem(new RareDropsOddsItem(0.75 * oddsMultiplier, Material.IRON_CHESTPLATE));
 				odds.addOddsItem(new RareDropsOddsItem(0.75 * oddsMultiplier, Material.IRON_HELMET));
 				odds.addOddsItem(new RareDropsOddsItem(0.75 * oddsMultiplier, Material.IRON_LEGGINGS));
-				odds.addOddsItem(new RareDropsOddsItem(0.02 * oddsMultiplier, Material.DIAMOND_CHESTPLATE));
-				odds.addOddsItem(new RareDropsOddsItem(0.02 * oddsMultiplier, Material.DIAMOND_HELMET));
-				odds.addOddsItem(new RareDropsOddsItem(0.02 * oddsMultiplier, Material.DIAMOND_LEGGINGS));
-				odds.addOddsItem(new RareDropsOddsItem(0.02 * oddsMultiplier, Material.DIAMOND_BOOTS));
+				odds.addOddsItem(new RareDropsOddsItem(0.03 * oddsMultiplier, Material.DIAMOND_CHESTPLATE));
+				odds.addOddsItem(new RareDropsOddsItem(0.03 * oddsMultiplier, Material.DIAMOND_HELMET));
+				odds.addOddsItem(new RareDropsOddsItem(0.03 * oddsMultiplier, Material.DIAMOND_LEGGINGS));
+				odds.addOddsItem(new RareDropsOddsItem(0.03 * oddsMultiplier, Material.DIAMOND_BOOTS));
 				odds.addOddsItem(new RareDropsOddsItem(0.01 * oddsMultiplier, Material.RECORD_3));
 				odds.addOddsItem(new RareDropsOddsItem(0.01 * oddsMultiplier, Material.RECORD_4));
 				odds.addOddsItem(new RareDropsOddsItem(0.01 * oddsMultiplier, Material.RECORD_5));
@@ -122,17 +121,70 @@ public class RareDropsFactory
 				odds.addOddsItem(new RareDropsOddsItem(1.00 * oddsMultiplier, Material.STONE));
 				odds.addOddsItem(new RareDropsOddsItem(1.00 * oddsMultiplier, Material.BLAZE_POWDER));
 				odds.addOddsItem(new RareDropsOddsItem(0.50 * oddsMultiplier, Material.GOLDEN_APPLE));
-				odds.addOddsItem(new RareDropsOddsItem(0.75 * oddsMultiplier, Material.IRON_BOOTS));
-				odds.addOddsItem(new RareDropsOddsItem(0.75 * oddsMultiplier, Material.IRON_CHESTPLATE));
-				odds.addOddsItem(new RareDropsOddsItem(0.75 * oddsMultiplier, Material.IRON_HELMET));
-				odds.addOddsItem(new RareDropsOddsItem(0.75 * oddsMultiplier, Material.IRON_LEGGINGS));
-				odds.addOddsItem(new RareDropsOddsItem(0.02 * oddsMultiplier, Material.DIAMOND_CHESTPLATE));
-				odds.addOddsItem(new RareDropsOddsItem(0.02 * oddsMultiplier, Material.DIAMOND_HELMET));
-				odds.addOddsItem(new RareDropsOddsItem(0.02 * oddsMultiplier, Material.DIAMOND_LEGGINGS));
-				odds.addOddsItem(new RareDropsOddsItem(0.02 * oddsMultiplier, Material.DIAMOND_BOOTS));
+				odds.addOddsItem(new RareDropsOddsItem(0.50 * oddsMultiplier, Material.IRON_BOOTS));
+				odds.addOddsItem(new RareDropsOddsItem(0.50 * oddsMultiplier, Material.IRON_CHESTPLATE));
+				odds.addOddsItem(new RareDropsOddsItem(0.50 * oddsMultiplier, Material.IRON_HELMET));
+				odds.addOddsItem(new RareDropsOddsItem(0.50 * oddsMultiplier, Material.IRON_LEGGINGS));
+				odds.addOddsItem(new RareDropsOddsItem(0.05 * oddsMultiplier, Material.DIAMOND_CHESTPLATE));
+				odds.addOddsItem(new RareDropsOddsItem(0.05 * oddsMultiplier, Material.DIAMOND_HELMET));
+				odds.addOddsItem(new RareDropsOddsItem(0.05 * oddsMultiplier, Material.DIAMOND_LEGGINGS));
+				odds.addOddsItem(new RareDropsOddsItem(0.05 * oddsMultiplier, Material.DIAMOND_BOOTS));
 				odds.addOddsItem(new RareDropsOddsItem(0.25 * oddsMultiplier, Material.DIAMOND_SWORD));
 				odds.addOddsItem(new RareDropsOddsItem(5.00 * oddsMultiplier, Material.MELON));
 				break;	
+			}
+			case EntityUtilities.Slime:
+			{
+				this.logger.info("Killed Slime");
+				odds.addOddsItem(new RareDropsOddsItem(0.25 * oddsMultiplier, Material.CAKE));
+				odds.addOddsItem(new RareDropsOddsItem(5.00 * oddsMultiplier, Material.CLAY));
+				odds.addOddsItem(new RareDropsOddsItem(0.50 * oddsMultiplier, Material.MAP));
+				odds.addOddsItem(new RareDropsOddsItem(0.50 * oddsMultiplier, Material.GOLDEN_APPLE));
+				odds.addOddsItem(new RareDropsOddsItem(0.01 * oddsMultiplier, Material.RECORD_3));
+				odds.addOddsItem(new RareDropsOddsItem(0.01 * oddsMultiplier, Material.RECORD_4));
+				odds.addOddsItem(new RareDropsOddsItem(0.01 * oddsMultiplier, Material.RECORD_5));
+				odds.addOddsItem(new RareDropsOddsItem(0.01 * oddsMultiplier, Material.RECORD_6));
+				odds.addOddsItem(new RareDropsOddsItem(0.01 * oddsMultiplier, Material.RECORD_7));
+				odds.addOddsItem(new RareDropsOddsItem(0.01 * oddsMultiplier, Material.RECORD_8));
+				odds.addOddsItem(new RareDropsOddsItem(0.01 * oddsMultiplier, Material.RECORD_9));
+				odds.addOddsItem(new RareDropsOddsItem(0.01 * oddsMultiplier, Material.RECORD_10));
+				odds.addOddsItem(new RareDropsOddsItem(0.01 * oddsMultiplier, Material.RECORD_11));	
+				break;
+			}
+			case EntityUtilities.CaveSpider:
+			{
+				this.logger.info("Killed Spider");
+				odds.addOddsItem(new RareDropsOddsItem(6.00 * oddsMultiplier, Material.BREAD));
+				odds.addOddsItem(new RareDropsOddsItem(3.00 * oddsMultiplier, Material.COOKIE));
+				odds.addOddsItem(new RareDropsOddsItem(2.00 * oddsMultiplier, Material.LAPIS_ORE));
+				odds.addOddsItem(new RareDropsOddsItem(1.00 * oddsMultiplier, Material.INK_SACK));
+				odds.addOddsItem(new RareDropsOddsItem(3.00 * oddsMultiplier, Material.RAILS));
+				break;
+			}
+			case EntityUtilities.EnderDragon:
+			{
+				this.logger.info("Killed Enderdragon");
+				odds.addOddsItem(new RareDropsOddsItem(5.00 * oddsMultiplier, Material.GOLDEN_APPLE));
+				odds.addOddsItem(new RareDropsOddsItem(7.50 * oddsMultiplier, Material.IRON_BOOTS));
+				odds.addOddsItem(new RareDropsOddsItem(7.50 * oddsMultiplier, Material.IRON_CHESTPLATE));
+				odds.addOddsItem(new RareDropsOddsItem(7.50 * oddsMultiplier, Material.IRON_HELMET));
+				odds.addOddsItem(new RareDropsOddsItem(7.50 * oddsMultiplier, Material.IRON_LEGGINGS));
+				odds.addOddsItem(new RareDropsOddsItem(1.00 * oddsMultiplier, Material.DIAMOND_CHESTPLATE));
+				odds.addOddsItem(new RareDropsOddsItem(1.00 * oddsMultiplier, Material.DIAMOND_HELMET));
+				odds.addOddsItem(new RareDropsOddsItem(1.00 * oddsMultiplier, Material.DIAMOND_LEGGINGS));
+				odds.addOddsItem(new RareDropsOddsItem(1.00 * oddsMultiplier, Material.DIAMOND_BOOTS));
+				odds.addOddsItem(new RareDropsOddsItem(3.00 * oddsMultiplier, Material.DIAMOND_PICKAXE));
+				odds.addOddsItem(new RareDropsOddsItem(3.00 * oddsMultiplier, Material.DIAMOND_SPADE));
+				odds.addOddsItem(new RareDropsOddsItem(3.00 * oddsMultiplier, Material.DIAMOND_AXE));
+				odds.addOddsItem(new RareDropsOddsItem(3.00 * oddsMultiplier, Material.DIAMOND_HOE));
+				odds.addOddsItem(new RareDropsOddsItem(1.50 * oddsMultiplier, Material.DIAMOND_SWORD));
+				odds.addOddsItem(new RareDropsOddsItem(7.50 * oddsMultiplier, Material.IRON_PICKAXE));
+				odds.addOddsItem(new RareDropsOddsItem(7.50 * oddsMultiplier, Material.IRON_AXE));
+				odds.addOddsItem(new RareDropsOddsItem(7.50 * oddsMultiplier, Material.IRON_SWORD));
+				odds.addOddsItem(new RareDropsOddsItem(7.50 * oddsMultiplier, Material.IRON_HOE));
+				odds.addOddsItem(new RareDropsOddsItem(7.50 * oddsMultiplier, Material.IRON_SPADE));
+				break;
+				break;
 			}
 			default:
 			{
