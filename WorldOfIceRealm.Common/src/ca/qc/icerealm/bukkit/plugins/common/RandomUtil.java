03176eb2413f5de422810e8a4e8072347d2dadf5
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-05 17:08:19 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@62 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

--- WorldOfIceRealm.Common/src/ca/qc/icerealm/bukkit/plugins/common/RandomUtil.java
+++ WorldOfIceRealm.Common/src/ca/qc/icerealm/bukkit/plugins/common/RandomUtil.java
@@ -18,9 +18,28 @@ public class RandomUtil {
 		return random.doubleValue();
 	}
 	
+	/*
+	 * Renvoie un nombre au hasard entre 0 et max
+	 */
 	public static int getRandomInt(int max) {
 		Random r = new Random();
 		return r.nextInt(max);
 	}
 	
+	/*
+	 * Permet d'obtenir le resultat d'un tirage. Un premier nombre est généré
+	 * au hasard (entre 0 et number). Ensuite, un deuxieme nombre est généré et
+	 * s'ils sont égaux, renvoie true, sinon false.
+	 * Utile pour calculer des probabilités basé sur 1 chance sur N
+	 */
+	public static boolean getDrawResult(int number) {
+		// on genere un nombre au hasard de 0 a N
+		int magicNumber = RandomUtil.getRandomInt(number);
+		
+		// on refait un random
+		int pickedNumber = RandomUtil.getRandomInt(number);
+		
+		// si les deux sont pareils, on a gagné!
+		return magicNumber == pickedNumber;
+	}
 }
