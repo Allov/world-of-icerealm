e486a721cd31b305b880b5431b995c5a77425eb2
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-07 21:29:50 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@86 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

--- WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/WalkingScenario.java
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/WalkingScenario.java
@@ -8,13 +8,21 @@ import ca.qc.icerealm.bukkit.plugins.scenarios.core.Scenario;
 public class WalkingScenario extends Scenario {
 		
 	private boolean _active = false;
+	private long _started;
+	private long _elapsed = 30000;
+	private long _created;
+	private boolean _cannotBeStartedEver = false;
 	
-	public WalkingScenario() { }
+	public WalkingScenario() { 
+		_created = System.currentTimeMillis();
+	}
 
 	@Override
 	public void triggerScenario() {
 		_active = true;
+		_started = System.currentTimeMillis();
 		getServer().broadcastMessage("The walking scenario is triggered!!!");
+		
 		
 	}
 
@@ -23,25 +31,41 @@ public class WalkingScenario extends Scenario {
 		return _active;
 	}
 	
-	@Override 
-	public boolean isComplete() {
-		return false;
-	}
+
 
 	@Override
 	public void abortScenario() {
 		_active = false;
+		_cannotBeStartedEver = true;
 		getServer().broadcastMessage("The Walking scenario has been abort");	
 	}
 
 	@Override
 	public boolean abortWhenLeaving() {
 		// TODO Auto-generated method stub
-		return true;
+		return false;
 	}
 
 	@Override
 	public boolean canBeTriggered() {
+		return (_created + _elapsed) < System.currentTimeMillis() && !_cannotBeStartedEver;
+	}
+
+	@Override
+	public boolean mustBeStop() {
+		return (_started + _elapsed) < System.currentTimeMillis();
+	}
+
+	@Override
+	public void terminateScenario() {
+		// TODO Auto-generated method stub
+		getServer().broadcastMessage("The Walking scenario has been terminated");	
+		_active = false;
+		_cannotBeStartedEver = true;
+	}
+
+	@Override
+	public boolean isComplete() {
 		// TODO Auto-generated method stub
 		return false;
 	}
