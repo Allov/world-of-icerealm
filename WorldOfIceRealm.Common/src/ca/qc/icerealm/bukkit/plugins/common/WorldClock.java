90c6542acb80d27ee69ca9b7f629c3882d1a736f
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-08 01:47:03 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@93 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

---
+++ WorldOfIceRealm.Common/src/ca/qc/icerealm/bukkit/plugins/common/WorldClock.java
@@ -0,0 +1,16 @@
+package ca.qc.icerealm.bukkit.plugins.common;
+
+import java.util.logging.Logger;
+
+import org.bukkit.World;
+
+public class WorldClock {
+
+	public final static Logger logger = Logger.getLogger(("Minecraft"));
+	
+	public static int getHour(World w) {
+		return (int)(w.getTime() / 1000);
+		
+	}
+	
+}
