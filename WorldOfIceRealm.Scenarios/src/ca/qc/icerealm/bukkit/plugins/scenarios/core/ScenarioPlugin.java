62a777d48b7fd074c09902d3aa922c4c1ed72b90
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-14 01:32:15 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@154 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

--- WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/core/ScenarioPlugin.java
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/core/ScenarioPlugin.java
@@ -2,19 +2,22 @@ package ca.qc.icerealm.bukkit.plugins.scenarios.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import ca.qc.icerealm.bukkit.plugins.scenarios.waves.MonsterFury;
+import ca.qc.icerealm.bukkit.plugins.scenarios.monsterfury.MonsterFury;
 
 
 public class ScenarioPlugin extends JavaPlugin {
 	
+	MonsterFury _monster;
+	
 	@Override
 	public void onDisable() {
-		
+		_monster.removeAllListener();
 	}
 
 	@Override
 	public void onEnable() {
-		MonsterFury fury = new MonsterFury(this);
+		_monster = new MonsterFury(this);
+		
 		
 	}
 }
