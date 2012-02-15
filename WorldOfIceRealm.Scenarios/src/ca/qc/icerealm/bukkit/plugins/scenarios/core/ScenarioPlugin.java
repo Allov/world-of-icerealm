70c992b08519ee22cc6d3fc853ad01a9f72bd238
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-15 00:45:22 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@162 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

--- WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/core/ScenarioPlugin.java
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/core/ScenarioPlugin.java
@@ -1,18 +1,16 @@
 package ca.qc.icerealm.bukkit.plugins.scenarios.core;
 
-import java.util.List;
-
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import ca.qc.icerealm.bukkit.plugins.scenarios.monsterfury.MonsterFury;
-import ca.qc.icerealm.bukkit.plugins.scenarios.monsterfury.MonsterFuryEventsListener;
+import ca.qc.icerealm.bukkit.plugins.scenarios.monsterfury.MonsterFuryConfiguration;
+
 
 
 public class ScenarioPlugin extends JavaPlugin {
 	
-	MonsterFury _monster;
+	private MonsterFury _monster;
+	private MonsterFuryConfiguration _defaultConfig;
+	
 	
 	@Override
 	public void onDisable() {
@@ -21,72 +19,32 @@ public class ScenarioPlugin extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		_monster = new MonsterFury(this, new MyOwnEventListenenr());
+		// set les scénarios pré enregistré ici!
+		_defaultConfig = new MonsterFuryConfiguration();
+		_defaultConfig.ExperienceReward = 100;								// 100 level d'exp
+		_defaultConfig.CoolDownTime = 10000; 								// 10 sec
+		_defaultConfig.InitialTimeBeforeFirstWave = 10000;					// 10 sec
+		_defaultConfig.MinimumPlayer = 1;									// 1 joueur requis
+		_defaultConfig.MonstersPerWave = 10;								// 10 monstres par wave
+		_defaultConfig.Name = "Gladiatore";									// le nom du scénario 
+		_defaultConfig.NumberOfWaves = 3;									// 3 waves
+		_defaultConfig.TimeBetweenWave = 10000;								// 10 sec
+		_defaultConfig.ActivationZoneCoords = "-180,134,-177,137,0,128";	// zone d'Activation
+		_defaultConfig.ScenarioZoneCoords = "-189,127,-168,140,0,128";		// zone du scenario
 		
-		
+		_monster = new MonsterFury(this, _defaultConfig);
 	}
 }
 
-class MyOwnEventListenenr implements MonsterFuryEventsListener {
-
-	private int _nbMonster;
-	private int _nbMonsterLeft;
-	private int _nbWaves;
-	private List<Player> _player;
-
-	
-	@Override
-	public void waveStarting(int monster) {
-		_nbMonster = monster;
-		sendMessageToPlayers("This wave has " + monster + " monsters!"); 
-	}
-
-	@Override
-	public void waveIsDone(int done) {
-		sendMessageToPlayers("This wave has been pushed back! " + (_nbWaves - done) + " waves left.");
-	}
 
 
-	@Override
-	public void scenarioEnding(int wavedone) {
-		sendMessageToPlayers("The enemy has been defeated! " + wavedone + " waves have been pushed back!");
-		
-	}
 
-	@Override
-	public void scenarioAborting(int wavedone) {
-		sendMessageToPlayers("You did " + wavedone + "/" + _nbWaves);
-		sendMessageToPlayers(_nbMonsterLeft + " monsters left");
-		sendMessageToPlayers("Scenario has been aborted");
-	}
 
-	@Override
-	public void coolDownChanged(boolean value) {
-		// rien a faire! 
-	}
+/*
 
-	@Override
-	public void playerDied(Player p, List<Player> players) {
-		sendMessageToPlayers(p.getName() + " died! " + _player.size() + " still alive!");
-	}
 
-	@Override
-	public void monsterDied(Entity e, int left) {
-		_nbMonsterLeft = left;
-		sendMessageToPlayers(_nbMonster - left + "/" + _nbMonster + " killed");
-	}
-
-	@Override
-	public void scenarioStarting(int waves, List<Player> list) {
-		_nbWaves = waves;
-		_player = list;
-		sendMessageToPlayers(waves + " waves are coming. We are " + _player.size() + " fighters");
-	}
-	
-	private void sendMessageToPlayers(String info) {
-		for (Player p : _player) {
-			p.sendMessage(info);
-		}
-	}
-	
-}
+//_activationZone = new WorldZone(_world, "-180,134,-177,137,0,128");
+//_scenarioZone = new WorldZone(_world, "-189,127,-168,140,0,128");
+//_activationZone = new WorldZone(_world, "147,-405,149,-403,0,128");
+//_scenarioZone = new WorldZone(_world, "137,-416,155,-399,0,128");
+*/
