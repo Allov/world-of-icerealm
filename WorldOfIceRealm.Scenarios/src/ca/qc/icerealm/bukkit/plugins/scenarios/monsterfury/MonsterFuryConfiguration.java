ff580e6fc99b4011288a14e774ab7bfb9a7a77b8
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-15 01:15:07 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@165 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

---
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/monsterfury/MonsterFuryConfiguration.java
@@ -0,0 +1,14 @@
+package ca.qc.icerealm.bukkit.plugins.scenarios.monsterfury;
+
+public class MonsterFuryConfiguration {
+	public long CoolDownTime;
+	public int MinimumPlayer;
+	public String ActivationZoneCoords;
+	public String ScenarioZoneCoords;
+	public int ExperienceReward;
+	public int NumberOfWaves;
+	public int MonstersPerWave;
+	public long TimeBetweenWave;
+	public long InitialTimeBeforeFirstWave;
+	public String Name;
+}
