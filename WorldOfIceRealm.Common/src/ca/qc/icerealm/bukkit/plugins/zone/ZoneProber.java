baada28cd3cc1e1217f7caa040c29c2508b0e074
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-15 18:46:50 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@175 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

--- WorldOfIceRealm.Common/src/ca/qc/icerealm/bukkit/plugins/zone/ZoneProber.java
+++ WorldOfIceRealm.Common/src/ca/qc/icerealm/bukkit/plugins/zone/ZoneProber.java
@@ -1,6 +1,8 @@
 package ca.qc.icerealm.bukkit.plugins.zone;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Logger;
 import org.bukkit.entity.Player;
 
@@ -8,9 +10,12 @@ public class ZoneProber implements Runnable {
 	public final Logger logger = Logger.getLogger(("Minecraft"));
 	private boolean _stop = false;
 	private HashMap<Player, ZoneObserver> _playersInZone;
+	private HashMap<ZoneObserver, List<Player>> _zoneObservers;
+	private boolean _mutex = false;
 	
 	public ZoneProber() {
 		_playersInZone = new HashMap<Player, ZoneObserver>();
+		_zoneObservers = new HashMap<ZoneObserver, List<Player>>();
 	}
 		
 	@Override
@@ -23,13 +28,60 @@ public class ZoneProber implements Runnable {
 				Player[] players = zone.getCurrentServer().getOnlinePlayers();
 				
 				for (Player p : players) {
-					if (zone.getWorldZone().isInside(p.getLocation()) && !_playersInZone.containsKey(p)) {
-						zone.playerEntered(p);
-						_playersInZone.put(p, zone);
+					if (zone.getWorldZone().isInside(p.getLocation())) {
+						
+						
+						
+						if (_zoneObservers.containsKey(zone) && _zoneObservers.get(zone) != null) {
+							
+							if (!_zoneObservers.get(zone).contains(p)) {
+								this.logger.info("must add plauyer");
+								zone.playerEntered(p);
+								_zoneObservers.get(zone).add(p);
+							}
+
+						}
+						else if (_zoneObservers.get(zone) == null) {
+							this.logger.info("the list of player need to be created!");
+							zone.playerEntered(p);
+							List<Player> list = new ArrayList<Player>();
+							list.add(p);
+							_zoneObservers.put(zone, list);
+						}
+						
 					}
 				}
+				
+				if (_zoneObservers != null &&  _zoneObservers.get(zone) != null && _zoneObservers.get(zone).size() > 0) {
+		
+					List<Player> listcopy = new ArrayList<Player>();
+					for (Player copy : _zoneObservers.get(zone)) {
+						listcopy.add(copy);
+					}
+					
+					for (Player p : listcopy) {
+						if (!zone.getWorldZone().isInside(p.getLocation())) {
+							zone.playerLeft(p);
+							this.logger.info("Must remove player from zone");
+							_zoneObservers.get(zone).remove(p);
+							this.logger.info("After remove player from zone");
+						}
+					}
+				}
+				
+				
+				
+				
+			}
+			/*
+			for (ZoneObserver obs : _zoneObservers.keySet()) {
+				
+
 			}
 			
+			*/
+			
+			/*
 			// détecte la sortie d'un joueur d'une zone
 			for (Player p : _playersInZone.keySet()) {
 				ZoneObserver zone = _playersInZone.get(p);
@@ -38,6 +90,7 @@ public class ZoneProber implements Runnable {
 					_playersInZone.remove(p);
 				}
 			}
+			*/
 			
 			try {
 				Thread.sleep(100);
