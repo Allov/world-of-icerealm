fc1afc5e2a6c091abd1c97b55c0c7d73a422f1f8
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-18 02:57:25 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@192 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

---
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/monsterfury/FourCornerWave.java
@@ -0,0 +1,117 @@
+package ca.qc.icerealm.bukkit.plugins.scenarios.monsterfury;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.CreatureType;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Monster;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+import ca.qc.icerealm.bukkit.plugins.common.EntityUtilities;
+import ca.qc.icerealm.bukkit.plugins.common.RandomUtil;
+import ca.qc.icerealm.bukkit.plugins.common.WorldZone;
+import ca.qc.icerealm.bukkit.plugins.scenarios.core.Scenario;
+import ca.qc.icerealm.bukkit.plugins.time.TimeObserver;
+import ca.qc.icerealm.bukkit.plugins.time.TimeServer;
+
+public class FourCornerWave implements EntityWave, TimeObserver {
+
+	private WorldZone _zone;
+	private World _world;
+	private List<LivingEntity> _monsters;
+	private Scenario _scenario;
+	private String[] possibleMonsters = new String[] { "zombie", "skeleton", "spider" };
+	private int _maxMonster;
+	private int _radius;
+	private long _alarm;
+	private long _delayBetweenSpawn;
+	
+	public FourCornerWave(WorldZone z, World w, Scenario s, int maxMonster, int radius, long delay) {
+		_zone = z;
+		_world = w;
+		_scenario = s;
+		_monsters = new ArrayList<LivingEntity>();
+		_maxMonster = maxMonster;
+		_radius = radius;
+		_delayBetweenSpawn = delay;
+	}
+	
+	@Override
+	public void spawnWave() {
+		
+		if (_monsters.size() <= _maxMonster) {
+			List<Location> corners = _zone.getFourCorner(_radius);
+			for (int i = 0; i < corners.size(); i++) {
+				CreatureType type = EntityUtilities.getCreatureType(possibleMonsters[RandomUtil.getRandomInt(possibleMonsters.length)]);		
+				_monsters.add(_world.spawnCreature(corners.get(i), type));
+			}
+			
+			// c'est une wave qui spawn des mosntres a chaque demi secon (500ms)
+			TimeServer.getInstance().addListener(this, _delayBetweenSpawn);
+		}
+	}
+
+	@Override
+	public void processEntityDeath(Entity e) {
+		// TODO Auto-generated method stub
+		if (_scenario.isActive()) {
+			if (e instanceof Monster) {
+				_monsters.remove((LivingEntity)e);
+			}
+		}
+		
+		
+	}
+
+	@Override
+	public void processDamage(EntityDamageEvent e) {
+		if (_scenario.isActive()) {
+			if (_monsters != null && _monsters.contains(e.getEntity())) {
+				if (e.getCause() == DamageCause.FIRE_TICK) {
+					e.setCancelled(true);
+				}	
+			}
+		}
+	}
+
+	@Override
+	public void cancelWave() {
+		// TODO Auto-generated method stub
+		if (_scenario.isActive()) {
+			for (LivingEntity e : _monsters) {
+				e.remove();
+			}
+		}
+		
+		TimeServer.getInstance().removeListener(this);
+	}
+
+	@Override
+	public int getNbOfEntities() {
+		return _monsters.size();
+	}
+
+
+	@Override
+	public void timeHasCome(long time) {
+		this.spawnWave();
+	}
+
+
+	@Override
+	public void setAlaram(long time) {
+		_alarm = time;
+	}
+
+
+	@Override
+	public long getAlarm() {
+		// TODO Auto-generated method stub
+		return _alarm;
+	}
+}
---
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/monsterfury/PlayerOutTimer.java
@@ -0,0 +1,40 @@
+package ca.qc.icerealm.bukkit.plugins.scenarios.monsterfury;
+
+import org.bukkit.entity.Player;
+
+import ca.qc.icerealm.bukkit.plugins.common.WorldZone;
+import ca.qc.icerealm.bukkit.plugins.scenarios.core.Scenario;
+import ca.qc.icerealm.bukkit.plugins.time.TimeObserver;
+
+public class PlayerOutTimer implements TimeObserver {
+
+	private long _alarm;
+	private Player _player;
+	private WorldZone _zone;
+	private Scenario _scenario;
+	
+	public PlayerOutTimer(Player p, WorldZone z, Scenario s) {
+		_player = p;
+		_zone = z;
+		_scenario = s;
+	}
+	
+	@Override
+	public void timeHasCome(long time) {
+		if (!_zone.isInside(_player.getLocation())) {
+			_scenario.removePlayerFromScenario(_player);
+		}
+	}
+
+	@Override
+	public void setAlaram(long time) {
+		_alarm = time;		
+	}
+
+	@Override
+	public long getAlarm() {
+		// TODO Auto-generated method stub
+		return _alarm;
+	}
+
+}
