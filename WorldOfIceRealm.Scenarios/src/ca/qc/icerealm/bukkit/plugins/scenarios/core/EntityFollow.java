ab7e8b2376cc2e5d6aa22a33be8cc4deccdc29f4
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-22 22:50:57 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@239 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

---
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/core/EntityFollow.java
@@ -0,0 +1,81 @@
+package ca.qc.icerealm.bukkit.plugins.scenarios.core;
+
+import org.bukkit.Location;
+import org.bukkit.Server;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.NPC;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+
+import ca.qc.icerealm.bukkit.plugins.common.WorldZone;
+import ca.qc.icerealm.bukkit.plugins.time.TimeObserver;
+import ca.qc.icerealm.bukkit.plugins.time.TimeServer;
+import ca.qc.icerealm.bukkit.plugins.zone.ZoneObserver;
+
+public class EntityFollow implements TimeObserver, ZoneObserver {
+
+	private long _alarm;
+	private Villager _follower;
+	private LivingEntity _followee;
+	private WorldZone _zone;
+	private boolean _isFollowing = false;
+	private Location _startingPosition;
+	
+	public EntityFollow(LivingEntity e) {
+		_follower = (Villager)e;
+			
+		
+		_startingPosition = e.getLocation();
+		_zone = new WorldZone(e.getLocation(), 4);
+		timeHasCome(System.currentTimeMillis());
+	}
+	
+	@Override
+	public void timeHasCome(long time) {
+		if (_isFollowing) {
+			_follower.setTarget(_followee);
+		}
+		else {
+			_follower.teleport(_startingPosition);
+		}
+		
+		TimeServer.getInstance().addListener(this, 100);
+	}
+
+	@Override
+	public void setAlaram(long time) {
+		_alarm = time;
+	}
+
+	@Override
+	public long getAlarm() {
+		return _alarm;
+	}
+
+	@Override
+	public void setWorldZone(WorldZone z) {
+		_zone = z;
+	}
+
+	@Override
+	public WorldZone getWorldZone() {
+		return _zone;
+	}
+
+	@Override
+	public void playerEntered(Player p) {
+		_followee = p;
+		_isFollowing = true;
+	}
+
+	@Override
+	public void playerLeft(Player p) {
+		
+	}
+
+	@Override
+	public Server getCurrentServer() {
+		return _follower.getServer();
+	}
+
+}
---
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/core/StrongerMonster.java
@@ -0,0 +1,36 @@
+package ca.qc.icerealm.bukkit.plugins.scenarios.core;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.CreatureType;
+import org.bukkit.entity.Monster;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class StrongerMonster implements Listener {
+	public final Logger logger = Logger.getLogger(("Minecraft"));
+	private Location _loc;
+	private CreatureType _type;
+	private int _max;
+	private int _entityId;
+	
+	public StrongerMonster(Monster m, int max) {
+		this.logger.info("StrongerMonster class");
+		_max = max;
+		_entityId = m.getEntityId();
+	}
+	
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onMonsterDamage(EntityDamageEvent e) {
+		if (_entityId == e.getEntity().getEntityId()) {
+			this.logger.info(String.valueOf(e.getDamage()));
+			
+		}
+	}
+	
+	
+}
