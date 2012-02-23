dd2ae0cace41486a0225287970d8a3c73c2c6865
leandreb <leandreb@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-23 00:54:52 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@242 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

---
+++ WorldOfIceRealm.Recipes/src/ca/qc/icerealm/bukkit/plugins/Recipes/Recipes.java
@@ -0,0 +1,21 @@
+package ca.qc.icerealm.bukkit.plugins.recipes;
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Recipes extends JavaPlugin {
+
+    public void onDisable() {
+
+
+
+    }
+
+    public void onEnable() {
+
+
+
+    }
+
+}
