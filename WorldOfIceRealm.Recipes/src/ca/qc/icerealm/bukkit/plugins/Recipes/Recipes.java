fafdbc36edc5e15eb816fbd545616d357cd25a54
leandreb <leandreb@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-23 01:51:02 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@245 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

--- WorldOfIceRealm.Recipes/src/ca/qc/icerealm/bukkit/plugins/Recipes/Recipes.java
+++ WorldOfIceRealm.Recipes/src/ca/qc/icerealm/bukkit/plugins/Recipes/Recipes.java
@@ -1,31 +1,23 @@
 package ca.qc.icerealm.bukkit.plugins.recipes;
 
-import java.lang.Character;
-import java.lang.Object;
-import java.util.ArrayList;
-
 import org.bukkit.Material;
-import org.bukkit.entity.*;
+import org.bukkit.Server;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import net.minecraft.server.CraftingManager;
 
 public class Recipes extends JavaPlugin {
 
-    //Logger log = Logger.getLogger("Minecraft");
-    CraftingManager manager = CraftingManager.getInstance();
-
-    private ArrayList _addedRecipes = new ArrayList();
+    public Server server;
 
     public void onEnable() {
 
-        // this.registerShapedRecipe(new ItemStack(Block.TNT, 1), new Object[] { "X#X", "#X#", "X#X", Character.valueOf('X'), Item.SULPHUR, Character.valueOf('#'), Block.SAND});
-        // this.registerShapelessRecipe(new ItemStack(Item.EYE_OF_ENDER, 1), new Object[] { Item.ENDER_PEARL, Item.BLAZE_POWDER});
-        
-        ItemStack _output = new ItemStack(Material.CLAY, 1);
+        server = getServer();
 
-        manager.registerShapedRecipe(_output, new Object[]{"XXX", "XXX", "XXX", Character.valueOf('X'), Item.SEEDS });
+        ShapedRecipe testOutput = new ShapedRecipe( new ItemStack( Material.GRAVEL, 1 ) );
+        testOutput.shape("SSS","SSS","SSS");
+        testOutput.setIngredient('S', Material.SEEDS);
+        server.addRecipe(testOutput);
 
     }
 
