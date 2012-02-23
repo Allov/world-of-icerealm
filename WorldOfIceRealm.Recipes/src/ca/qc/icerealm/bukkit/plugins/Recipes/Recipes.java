5d853604c40f09bdfa25a77e506e4a9155a5875e
leandreb <leandreb@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-23 01:42:41 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@244 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

--- WorldOfIceRealm.Recipes/src/ca/qc/icerealm/bukkit/plugins/Recipes/Recipes.java
+++ WorldOfIceRealm.Recipes/src/ca/qc/icerealm/bukkit/plugins/Recipes/Recipes.java
@@ -1,45 +1,36 @@
 package ca.qc.icerealm.bukkit.plugins.recipes;
 
+import java.lang.Character;
+import java.lang.Object;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.*;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.minecraft.server.CraftingManager;
 
-import java.lang.ArrayIndexOutOfBoundsException;
-
 public class Recipes extends JavaPlugin {
 
-    Logger log = Logger.getLogger("Minecraft");
+    //Logger log = Logger.getLogger("Minecraft");
     CraftingManager manager = CraftingManager.getInstance();
 
     private ArrayList _addedRecipes = new ArrayList();
 
     public void onEnable() {
 
-        _initialRecipeCount = manager.b().size();
-
         // this.registerShapedRecipe(new ItemStack(Block.TNT, 1), new Object[] { "X#X", "#X#", "X#X", Character.valueOf('X'), Item.SULPHUR, Character.valueOf('#'), Block.SAND});
         // this.registerShapelessRecipe(new ItemStack(Item.EYE_OF_ENDER, 1), new Object[] { Item.ENDER_PEARL, Item.BLAZE_POWDER});
         
         ItemStack _output = new ItemStack(Material.CLAY, 1);
 
-        manager.registerShapedRecipe(_output, new Object[] { "XXX", "XXX", "XXX", Character.valueOf('X'), Item.SEEDS });
-        //manager.registerShapelessRecipe(reward, new Object[]{ ItemStack.SEEDS, 2 });
+        manager.registerShapedRecipe(_output, new Object[]{"XXX", "XXX", "XXX", Character.valueOf('X'), Item.SEEDS });
 
     }
 
     public void onDisable() {
 
-        //manager.b().;
 
     }
 
