2359e13d82b8d4aee9742d8d7870fa8702fb38e2
pparadis <pparadis@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-24 21:59:13 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@267 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

---
+++ WorldOfIceRealm.Common/src/ca/qc/icerealm/bukkit/plugins/twitter/TwitterWrapper.java
@@ -0,0 +1,46 @@
+package ca.qc.icerealm.bukkit.plugins.twitter;
+
+import twitter4j.StatusUpdate;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterWrapper {
+	
+	private ConfigurationBuilder cb;
+	
+	public TwitterWrapper()
+	{
+		cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		.setOAuthConsumerKey("YEIaTiL6YtrC76M95Qt6g")
+		.setOAuthConsumerSecret("NX3zl9OCIu4eRqWvsIMVJWoW1IeTqrhaqvU4yDdD0")
+		.setOAuthAccessToken("490863314-RJBxwxSTh77zqW2Ur38RpZIQ6SrUX272wKyTeVTM")
+		.setOAuthAccessTokenSecret("ncefxX3S6FpM0Mkw3XSep7eTFU0F8nV6Druqkh4RPg");
+	}
+	
+	public void UpdateStatus(String status)
+	{
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+		
+		try {
+			twitter.updateStatus(new StatusUpdate(status));
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void SendDirectMessage(String username, String message)
+	{
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+		
+		try {
+			twitter.sendDirectMessage(username, message);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ WorldOfIceRealm.Common/src/ca/qc/icerealm/bukkit/plugins/twitter/test.java
@@ -0,0 +1,14 @@
+package ca.qc.icerealm.bukkit.plugins.twitter;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//new TwitterWrapper().UpdateStatus("test");
+		new TwitterWrapper().SendDirectMessage("pparadis", "");
+	}
+
+}
