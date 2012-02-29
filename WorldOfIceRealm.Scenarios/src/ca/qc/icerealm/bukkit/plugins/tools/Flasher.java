fe2d9fa739630664aa0799aa950ebedea9ba1e19
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-02-29 15:19:49 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@299 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

---
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/tools/Flasher.java
@@ -0,0 +1,54 @@
+package ca.qc.icerealm.bukkit.plugins.tools;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
+import ca.qc.icerealm.bukkit.plugins.time.TimeObserver;
+import ca.qc.icerealm.bukkit.plugins.time.TimeServer;
+
+public class Flasher implements TimeObserver {
+	public final Logger logger = Logger.getLogger(("Minecraft"));
+	private long _alarm;
+	
+	private boolean _showIt = false;
+	private Location _location;
+	private World _world;
+	
+	public Flasher(Location l) {
+		_location = l;
+		_world  = l.getWorld();
+	}
+	
+	
+	@Override
+	public void timeHasCome(long time) {
+		
+		Block b = _world.getBlockAt(_location);
+		if (_showIt) {
+			_showIt = false;
+			b.setType(Material.GLOWSTONE);
+		}
+		else {
+			_showIt = true;
+			b.setType(Material.AIR);
+		}
+		
+		TimeServer.getInstance().addListener(this, 1000);
+	}
+
+	@Override
+	public void setAlaram(long time) {
+		_alarm  = time;
+	}
+
+	@Override
+	public long getAlarm() {
+		return _alarm;
+	}
+
+	
+}
