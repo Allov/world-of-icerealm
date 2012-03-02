d0aad3731a47e648c6656f37ae97992a63d93428
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-03-02 00:50:38 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@315 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

---
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/tools/LoggerFormater.java
@@ -0,0 +1,25 @@
+package ca.qc.icerealm.bukkit.plugins.scenarios.tools;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Formattable;
+import java.util.Formatter;
+import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
+
+public class LoggerFormater extends SimpleFormatter {
+
+	@Override
+	public String format(LogRecord rec) {
+		Date timestamp = new Date(rec.getMillis());
+		StringBuffer buf = new StringBuffer();
+		buf.append(timestamp.getHours() + ":");
+		buf.append(timestamp.getMinutes() + ":");
+		if (timestamp.getSeconds() < 10) { buf.append("0"); }
+		buf.append(timestamp.getSeconds() + " ");
+		buf.append(rec.getLevel().toString() + " ");
+		buf.append(rec.getMessage() + "\n");
+		return buf.toString();
+	}
+
+}
