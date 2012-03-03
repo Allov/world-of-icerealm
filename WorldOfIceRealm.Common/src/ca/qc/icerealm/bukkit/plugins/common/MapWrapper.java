45218e94ad9191ba488dfab44ab04ec3238bba2d
Drask <Drask@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-03-03 04:00:21 +0000

    Ajout de getter (double et list)
    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@323 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

--- WorldOfIceRealm.Common/src/ca/qc/icerealm/bukkit/plugins/common/MapWrapper.java
+++ WorldOfIceRealm.Common/src/ca/qc/icerealm/bukkit/plugins/common/MapWrapper.java
@@ -1,14 +1,20 @@
 package ca.qc.icerealm.bukkit.plugins.common;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
 public class MapWrapper {
-	
+	public final Logger logger = Logger.getLogger(("RareMobsFactory"));
 	private final Map<String, Object> map;
 
 	public MapWrapper(Map<String, Object> map) {
 		this.map = map;		
+	}
+	
+	public Map<String, Object> getMap(){
+		return map;
 	}
 	
 	public int getInt(String key, int defaultIfNull) {
@@ -69,5 +75,28 @@ public class MapWrapper {
 		}
 		
 		return value;
+	}
+
+	
+	public List<MapWrapper> getMapList(String path, List<MapWrapper> defaultIfNull) {
+		if (map.get(path) == null) {
+			return defaultIfNull;
+		}
+		
+		List<MapWrapper> mapValues = null;
+		
+		try	{
+			ArrayList<Map<String, Object>> list = (ArrayList<Map<String, Object>>) map.get(path);
+			mapValues = new ArrayList<MapWrapper>();
+			
+			for (Map<String, Object> map : list) {
+				mapValues.add(new MapWrapper(map));
+			}
+		} catch(Exception exception) {
+			Logger.getLogger("MapWrapper").severe("Path: " + path + " is not a valid list of map.");
+			mapValues = defaultIfNull;
+		}
+		
+		return mapValues;
 	}
 }
