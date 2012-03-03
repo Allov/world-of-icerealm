2de5db8a390b01066480f158e2b14485f03a2146
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-03-03 20:28:52 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@337 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

---
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/tools/CustomMonsterListener.java
@@ -0,0 +1,67 @@
+package ca.qc.icerealm.bukkit.plugins.scenarios.tools;
+
+import java.util.HashMap;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Monster;
+import org.bukkit.event.EventHandler;
+
+
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+import ca.qc.icerealm.bukkit.plugins.scenarios.core.ScenarioPlugin;
+
+public class CustomMonsterListener implements Listener {
+
+	private HashMap<Integer, CustomMonster> _customMonsters;
+	
+	public CustomMonsterListener() {
+		_customMonsters = new HashMap<Integer, CustomMonster>();
+	}
+	
+	public void addMonster(Integer entityId, Integer health) {
+		CustomMonster c = new CustomMonster();
+		c.Health = health;
+		c.Burn = true;
+		_customMonsters.put(entityId, c);
+	}
+	
+	public void addMonster(Integer entityId, Integer health, boolean burn) {
+		CustomMonster c = new CustomMonster();
+		c.Health = health;
+		c.Burn = burn;
+		_customMonsters.put(entityId, c);
+	}
+	
+	public void removeMonster(Integer entityId) {
+		_customMonsters.remove(entityId);
+	}
+	
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onMonsterDamage(EntityDamageEvent event) {		
+		CustomMonster custom = _customMonsters.get(event.getEntity().getEntityId());
+		
+		if (custom != null && event.getEntity() instanceof Monster) {	
+			custom.Health -= event.getDamage();
+			Monster m = (Monster)event.getEntity();
+			
+			if (custom.Health < 0) {
+				m.damage(m.getMaxHealth());
+				_customMonsters.remove(event.getEntity().getEntityId());
+			}
+			else {
+				m.setHealth(m.getMaxHealth());
+			}
+		}
+	}
+}
+
+class CustomMonster {
+	public int EntityId;
+	public int Health;
+	public boolean Burn;
+	
+}
