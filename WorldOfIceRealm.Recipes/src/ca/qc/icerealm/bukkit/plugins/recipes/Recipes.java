080a3d13947808968209d8731c122fe1f50c3283
leandreb <leandreb@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-03-04 21:40:43 +0000

    Ajouté des choses à faire avec le Netherack.
    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@345 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

--- WorldOfIceRealm.Recipes/src/ca/qc/icerealm/bukkit/plugins/recipes/Recipes.java
+++ WorldOfIceRealm.Recipes/src/ca/qc/icerealm/bukkit/plugins/recipes/Recipes.java
@@ -9,21 +9,64 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Recipes extends JavaPlugin {
 
     public Server server;
-
+    
+    //private ArrayList<ShapedRecipe> customRecipes;
+    
     public void onEnable() {
 
         server = getServer();
 
-        ShapedRecipe testOutput = new ShapedRecipe( new ItemStack( Material.GRAVEL, 1 ) );
-        testOutput.shape("SSS","SSS","SSS");
-        testOutput.setIngredient('S', Material.SEEDS);
-        server.addRecipe(testOutput);
+        // Nether Brick 
+        ShapedRecipe netherBrickTL = new ShapedRecipe( new ItemStack( Material.NETHER_BRICK, 2 ) );
+        netherBrickTL.shape("NN ","NN ","   ");
+        netherBrickTL.setIngredient('N', Material.NETHERRACK);
+        server.addRecipe(netherBrickTL);
 
+        ShapedRecipe netherBrickTR = new ShapedRecipe( new ItemStack( Material.NETHER_BRICK, 2 ) );
+        netherBrickTR.shape(" NN"," NN","   ");
+        netherBrickTR.setIngredient('N', Material.NETHERRACK);
+        server.addRecipe(netherBrickTR);
+
+        ShapedRecipe netherBrickBL = new ShapedRecipe( new ItemStack( Material.NETHER_BRICK, 2 ) );
+        netherBrickBL.shape("   ","NN ","NN ");
+        netherBrickBL.setIngredient('N', Material.NETHERRACK);
+        server.addRecipe(netherBrickBL);
+
+        ShapedRecipe netherBrickBR = new ShapedRecipe( new ItemStack( Material.NETHER_BRICK, 2 ) );
+        netherBrickBR.shape("   "," NN"," NN");
+        netherBrickBR.setIngredient('N', Material.NETHERRACK);
+        server.addRecipe(netherBrickBR);
+        
+        // Nether Brick Stairs
+        ShapedRecipe netherBrickStairsL = new ShapedRecipe( new ItemStack( Material.NETHER_BRICK_STAIRS, 4 ) );
+        netherBrickStairsL.shape("N  ","NN ","NNN");
+        netherBrickStairsL.setIngredient('N', Material.NETHER_BRICK);
+        server.addRecipe(netherBrickStairsL);
+
+        ShapedRecipe netherBrickStairsR = new ShapedRecipe( new ItemStack( Material.NETHER_BRICK_STAIRS, 4 ) );
+        netherBrickStairsR.shape("  N"," NN","NNN");
+        netherBrickStairsR.setIngredient('N', Material.NETHER_BRICK);
+        server.addRecipe(netherBrickStairsR);
+
+        // Nether Brick Fences
+        ShapedRecipe netherBrickFenceT = new ShapedRecipe( new ItemStack( Material.NETHER_FENCE, 4 ) );
+        netherBrickFenceT.shape("NNN","NNN","   ");
+        netherBrickFenceT.setIngredient('N', Material.NETHER_BRICK);
+        server.addRecipe(netherBrickFenceT);
+
+        ShapedRecipe netherBrickFenceB = new ShapedRecipe( new ItemStack( Material.NETHER_FENCE, 4 ) );
+        netherBrickFenceB.shape("   ","NNN","NNN");
+        netherBrickFenceB.setIngredient('N', Material.NETHER_BRICK);
+        server.addRecipe(netherBrickFenceB);
+
+        //TODO: Add recipes to list of custom additions
+        
     }
 
     public void onDisable() {
-
-
+    	
+    	//TODO: Loop through list of custom additions to delete them.
+    	
     }
 
 }
