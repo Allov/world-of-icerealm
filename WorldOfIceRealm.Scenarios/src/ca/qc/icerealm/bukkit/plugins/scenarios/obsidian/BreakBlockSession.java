f684b379a958ffc8d6a49ea907d49da09e1a8a64
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-03-14 01:06:03 +0000

    doit briser tous les blocs d'obsidian pour tuer les montres autour
    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@369 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

--- WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/obsidian/BreakBlockSession.java
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/obsidian/BreakBlockSession.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Server;
@@ -16,14 +17,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
 import ca.qc.icerealm.bukkit.plugins.common.WorldZone;
 import ca.qc.icerealm.bukkit.plugins.scenarios.core.ScenarioPlugin;
 import ca.qc.icerealm.bukkit.plugins.scenarios.tools.CoolDown;
+import ca.qc.icerealm.bukkit.plugins.scenarios.tools.CoolDownTimer;
 import ca.qc.icerealm.bukkit.plugins.scenarios.tools.EntityHelper;
+import ca.qc.icerealm.bukkit.plugins.time.TimeServer;
 import ca.qc.icerealm.bukkit.plugins.zone.ZoneObserver;
 
 public class BreakBlockSession implements ZoneObserver, CoolDown {
-	private Logger _logger = ScenarioPlugin.logger;
+	private Logger _logger = Logger.getLogger("Minecraft");
 	private Location _locations;
 	private Material _material;
 	private double _radius;
@@ -36,6 +41,11 @@ public class BreakBlockSession implements ZoneObserver, CoolDown {
 	private boolean _blockBroken;
 	private boolean _isCooldownActive;
 	private long _coolDownTime;
+	private CoolDownTimer _coolDownTimer;
+	private long _coolDownDueTimestamp;
+	private String _header;
+	private boolean _resetWhenPlayerLeaving;
+	private String _msgWhenBlockBroken;
 	
 	public BreakBlockSession(Location l, Material m, double r, int qty, String monsters) {
 		_locations = l;
@@ -48,6 +58,17 @@ public class BreakBlockSession implements ZoneObserver, CoolDown {
 		_spawnedMonsters = new ArrayList<Monster>();
 		_blockBroken = true;
 		_isCooldownActive = false;
+		_coolDownTime = 0;
+		_header = ChatColor.LIGHT_PURPLE + "[" + ChatColor.DARK_PURPLE + "Obsidian" + ChatColor.LIGHT_PURPLE + "] ";
+		_resetWhenPlayerLeaving = false;
+	}
+	
+	public void setCooldownTime(long time) {
+		_coolDownTime = time;
+	}
+	
+	public void setResetWhenPlayerLeaving(boolean b) {
+		_resetWhenPlayerLeaving = b;
 	}
 	
 	public Location getBlockLocation() {
@@ -59,20 +80,29 @@ public class BreakBlockSession implements ZoneObserver, CoolDown {
 	}
 	
 	public void activateBreakingZone() {
-		for (int i = 0; i < _qty; i++) {
-			Location loc = _locations.clone();
-			loc.setY(loc.getY() + 2);
-			CreatureType t = EntityHelper.getRandomEntity(_monsters);
-			LivingEntity l = _world.spawnCreature(loc, t);
-			if (l instanceof Monster) {
-				_spawnedMonsters.add((Monster)l);
-				_logger.info(l.getEntityId() + " spawned at " + l.getLocation());
+		if (!_isCooldownActive && _blockBroken) {
+			_logger.info("activting");
+			for (int i = 0; i < _qty; i++) {
+				Location loc = _locations.clone();
+				loc.setY(loc.getY() + 2);
+				CreatureType t = EntityHelper.getRandomEntity(_monsters);
+				LivingEntity l = _world.spawnCreature(loc, t);
+				if (l instanceof Monster) {
+					_spawnedMonsters.add((Monster)l);
+					_logger.info(l.getEntityId() + " spawned at " + l.getLocation());
+				}
 			}
+			
+			org.bukkit.block.Block b = _world.getBlockAt(_locations);
+			b.setType(_material);		
+			_blockBroken = false;
+		}
+		else {
+			_logger.info("not activting");	
 		}
 		
-		org.bukkit.block.Block b = _world.getBlockAt(_locations);
-		b.setType(_material);		
-		_blockBroken = false;
+		
+		
 		
 	}
 	
@@ -82,15 +112,19 @@ public class BreakBlockSession implements ZoneObserver, CoolDown {
 		}
 		org.bukkit.block.Block b = _world.getBlockAt(_locations);
 		b.setType(Material.AIR);	
+		_blockBroken = true;
 	}
 
-	public void onBlockBroken(BlockBreakEvent event) {
+	public boolean onBlockBroken(BlockBreakEvent event) {
 		if (event.getBlock().getLocation().equals(_locations)) {
 			_blockBroken = true;
+			return true;
 		}
+		return false;
 	}
 	
 	public void onMonsterDamaged(EntityDamageEvent event) {
+		
 		Monster monster = null;
 		for (Monster m : _spawnedMonsters) {
 			if (m.getEntityId() == event.getEntity().getEntityId()) {
@@ -99,11 +133,25 @@ public class BreakBlockSession implements ZoneObserver, CoolDown {
 			}
 		}
 		
-		if (monster != null && !_blockBroken) {
-			monster.setHealth(monster.getMaxHealth());
-		}
-		else if (monster != null) {
-			monster.damage(event.getDamage());
+		
+		
+		if (monster != null) {
+			
+			if (event.getCause() == DamageCause.FIRE_TICK) {
+				event.setCancelled(true);
+				event.getEntity().setFireTicks(0);
+				// lette, hack, shame on me
+				return;
+			}
+			
+			if (!_blockBroken) {
+				monster.setHealth(monster.getMaxHealth());
+				//_logger.info("" + monster.getHealth());	
+			}
+			else {
+				monster.damage(event.getDamage());
+				//_logger.info("" + monster.getHealth() + " got damaged!!!");
+			}
 		}
 	}
 	
@@ -123,13 +171,40 @@ public class BreakBlockSession implements ZoneObserver, CoolDown {
 	}
 	
 	public void playerEntered(Player p) {
-		p.sendMessage("break the block of obsidian");
-		activateBreakingZone();
+		if (_isCooldownActive) {
+			p.sendMessage(ChatColor.GOLD + "Active in " + (_coolDownDueTimestamp - System.currentTimeMillis() + " ms"));
+		}
+		else {
+			
+			if (_blockBroken) {
+				p.sendMessage(_header + ChatColor.GREEN + "Break the Obsidian block");
+				activateBreakingZone();	
+			}
+			else {
+				p.sendMessage(ChatColor.GRAY + "entering the zone");
+			}
+			
+			if (_coolDownTime > 0) {
+				setCoolDownActive(true);
+				_coolDownTimer = new CoolDownTimer(this);
+				TimeServer.getInstance().addListener(_coolDownTimer, _coolDownTime);
+				_coolDownDueTimestamp = System.currentTimeMillis() + _coolDownTime;
+				_logger.info(_coolDownDueTimestamp + "cool down to be");
+			}
+			
+			
+			
+		}
 	}
 	
 	public void playerLeft(Player p) {
-		p.sendMessage("you are leaving!");
-		resetToInitialState();
+		if (_resetWhenPlayerLeaving) {
+			p.sendMessage(_header + ChatColor.RED + "You cancelled the project...");
+			resetToInitialState();	
+		}
+		
+		p.sendMessage(ChatColor.GRAY + "leaving the zone");
+		
 	}
 	
 	public Server getCurrentServer() {
--- WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/obsidian/ObsidianMission.java
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/obsidian/ObsidianMission.java
@@ -3,34 +3,52 @@ package ca.qc.icerealm.bukkit.plugins.scenarios.obsidian;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Monster;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.inventory.ItemStack;
+
+import ca.qc.icerealm.bukkit.plugins.common.RandomUtil;
 import ca.qc.icerealm.bukkit.plugins.zone.ZoneSubject;
 
-public class ObsidianMission {
+public class ObsidianMission implements Listener {
 
 	private Logger _logger = Logger.getLogger("Minecraft");
 	private List<BreakBlockSession> _breaks;
 	private ZoneSubject _zoneServer;
-
+	private int _blocksLeft;
+	private int[] _rewardObject;
 	
 	
 	public ObsidianMission(ZoneSubject zone) {
 		_zoneServer = zone;
-
+	}
+	
+	public void setReward(int[] ids) {
+		_rewardObject = ids;
+	}
+	
+	public void giveReward(Location l, int qty) {
+		
+		int id = _rewardObject[RandomUtil.getRandomInt(_rewardObject.length)];
+		l.getWorld().dropItemNaturally(l, new ItemStack(id, qty));
+		_blocksLeft = _breaks.size();
 	}
 
 	
 	public void setBreakBlockSession(List<BreakBlockSession> s) {
 		_breaks = s;
+		_blocksLeft = _breaks.size();
 	}
 	
 	public void activateBlockZone()  {
@@ -51,33 +69,24 @@ public class ObsidianMission {
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onBlockBreak(BlockBreakEvent event) {
 		for (BreakBlockSession s : _breaks) {
-			s.onBlockBroken(event);
+			if (s.onBlockBroken(event)) {
+				_blocksLeft--;
+				
+				if (_blocksLeft == 0) {
+					giveReward(s.getBlockLocation(), 1);
+					_logger.info("obsidian mission is over!");
+				}
+			}
 		}
-		/*
-		Material m = _blockToBreak.get(event.getBlock().getLocation());
-		if (m  != null) {
-			_logger.info("you did break a block at: " + event.getBlock().getLocation());
-			_blockToBreak.remove(event.getBlock().getLocation());
-			_logger.info(_blockToBreak.size() + " left to break!");
-		}
-		*/
 	}
 	
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onMonsterDamaged(EntityDamageEvent event) {
 
+		
 		for (BreakBlockSession s : _breaks) {
 			s.onMonsterDamaged(event);
 		}
-		
-		/*
-		Monster monster = null;
-		for (Monster m : _spawnedMonsters) {
-			if (m.getEntityId() == event.getEntity().getEntityId()) {
-				_logger.info("monster: " + m.getEntityId() + " received: " + event.getDamage() + " damage");
-			}
-		}
-		*/
 	}
 	
 	@EventHandler(priority = EventPriority.NORMAL)
@@ -86,15 +95,6 @@ public class ObsidianMission {
 		for (BreakBlockSession s : _breaks) {
 			s.onBlockDamage(event);
 		}
-		
-		/*
-		Monster monster = null;
-		for (Monster m : _spawnedMonsters) {
-			if (m.getEntityId() == event.getEntity().getEntityId()) {
-				_logger.info("monster: " + m.getEntityId() + " received: " + event.getDamage() + " damage");
-			}
-		}
-		*/
 	}
 	
 	@EventHandler(priority = EventPriority.NORMAL)
