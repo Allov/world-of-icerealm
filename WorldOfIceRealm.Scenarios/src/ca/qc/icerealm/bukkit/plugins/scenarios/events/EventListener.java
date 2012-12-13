c4ac7306d425f1de1015bc726b8458f92274d47f
punishermalade <punishermalade@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-12-13 00:24:19 +0000

    ajout de quelques evenements
    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@458 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

--- WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/events/EventListener.java
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/events/EventListener.java
@@ -2,4 +2,6 @@ package ca.qc.icerealm.bukkit.plugins.scenarios.events;
 
 public interface EventListener {
 	public void eventCompleted(Object ... param);	
+	public void eventActivated(Event e);
+	public void eventCleared();
 }
--- WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/events/EventService.java
+++ WorldOfIceRealm.Scenarios/src/ca/qc/icerealm/bukkit/plugins/scenarios/events/EventService.java
@@ -7,9 +7,11 @@ public class EventService {
 
 	private static EventService _instance = null;
 	private List<Event> _events;
+	private List<EventListener> _listener;
 	
 	protected EventService() {
 		_events = new ArrayList<Event>();
+		_listener = new ArrayList<EventListener>();
 	}
 	
 	public static EventService getInstance() {
@@ -25,6 +27,16 @@ public class EventService {
 	
 	public void addEvent(Event e) {
 		_events.add(e);
+		for (EventListener ev : _listener) {
+			ev.eventActivated(e);
+		}
+	}
+	
+	public void clearEvents() {
+		_events.clear();
+		for (EventListener ev : _listener) {
+			ev.eventCleared();
+		}
 	}
 	
 }
