b1610d12b5ddb1babfc6dbf487489b1270673cd4
Allov <Allov@8b22c5f6-99f7-4f02-aeed-e951476ab6e7>
2012-12-23 19:16:49 +0000

    git-svn-id: https://xp-dev.com/svn/WoIR/trunk@500 8b22c5f6-99f7-4f02-aeed-e951476ab6e7

---
+++ WorldOfIceRealm.Perks/src/ca/qc/icerealm/bukkit/plugins/perks/archer/LeatherExpertPerk.java
@@ -0,0 +1,66 @@
+package ca.qc.icerealm.bukkit.plugins.perks.archer;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+import ca.qc.icerealm.bukkit.plugins.perks.PerkService;
+
+public class LeatherExpertPerk implements Listener {
+
+	private PerkService perkService = PerkService.getInstance();
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onPlayerShoot(EntityDamageByEntityEvent evt) {
+		//if (evt.getEntity() instanceof Player && perkService.playerHasPerk((Player)evt.getEntity(), ArcherTree.LeatherExpertId)) {
+		
+		Player damager = null;
+		
+		if (evt.getDamager() instanceof Arrow) {
+			Arrow arrow = (Arrow)evt.getDamager();
+			if (arrow.getShooter() instanceof Player) {
+				damager = (Player)arrow.getShooter();
+			}
+		}
+		
+		if (damager != null && perkService.playerHasPerk((Player)damager, ArcherTree.LeatherExpertId)) {
+			PlayerInventory inventory = damager.getInventory();
+			ItemStack[] armorContents = inventory.getArmorContents();
+			
+			double damageBoost = 1;
+			
+			for(ItemStack armor : armorContents) {
+				if (armor.getType().equals(Material.LEATHER_BOOTS)) {
+					damageBoost += 0.10;
+				} else if (armor.getType().equals(Material.LEATHER_CHESTPLATE)) {
+					damageBoost += 0.10;
+				} else if (armor.getType().equals(Material.LEATHER_HELMET)) {
+					damageBoost += 0.10;
+				} else if (armor.getType().equals(Material.LEATHER_LEGGINGS)) {
+					damageBoost += 0.10;
+				}
+			}
+			
+			if (damageBoost == 0.4) {
+				damageBoost = 0.5;
+			}
+			
+			int baseDamage = evt.getDamage();
+			int finalDamage = (int) (baseDamage * damageBoost); 
+			evt.setDamage(finalDamage);
+			
+			Logger.getLogger("Minecraft").info("damage done: " + finalDamage + ", base damage: " + baseDamage + ", boost: " + damageBoost);
+		} else {
+			Logger.getLogger("Minecraft").info("damage done: " + evt.getDamage());
+		}
+	}
+	
+}
